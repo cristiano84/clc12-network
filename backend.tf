ddb2a80405057395df36190e9b5462d7561c0fab
EC2 Default User <ec2-user@ip-172-31-23-92.ec2.internal>
2024-10-15 23:13:37 +0000

    git bucket change name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-    bucket = "clc12-network-crisaquino"
+    bucket = "clc12-network-crisaquino2"
     key    = "state/terraform.tfstate"
     region = "us-east-1"
   }
